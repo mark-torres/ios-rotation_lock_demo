b196b7d4acb91ba72f45e6ecfc6198fc3ced4833
Mark Torres <mark.torres.mets@gmail.com>
2017-07-12 12:05:19 -0500

    Display views

---
+++ Orientation Lock Demo/LandscapeViewController.swift
@@ -0,0 +1,42 @@
+//
+//  LandscapeViewController.swift
+//  Orientation Lock Demo
+//
+//  Created by Marcos Torres on 7/12/17.
+//  Copyright © 2017 HSoft. All rights reserved.
+//
+
+import UIKit
+
+class LandscapeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+	
+	// MARK: - IB Actions
+	
+	@IBAction func tapClose(_ sender: Any) {
+		dismiss(animated: true, completion: nil)
+	}
+	
+
+}
